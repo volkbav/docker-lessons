646e425e48ac4bed5b80e5464eded898f80c2f53
volkbav <berezov86@gmail.com>
2026-02-23 18:00:09 +0300

    first commit

---
+++ docker-compose-app/app/main.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+MONGO_URL = "mongodb://mongo:27017"  # mongo - имя сервиса в докер
+client = MongoClient(MONGO_URL)
+db = client.admin  # admin - бд по умолчанию
+dbs_list = db.command("listDatabases")
+pprint(dbs_list)
---
+++ python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('добро пожаловать в календарь\n')
+
+year = int(input('введите год: '))
+month = int(input('введите  месяц: '))
+
+print(calendar.month(year, month))
+
+print('hasta la vista, babe')
